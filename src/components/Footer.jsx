136ddd224233f002c35dab8e41ee37920ac17cb9
Imran Tanbir <tanbirimran@gmail.com>
2022-12-22 17:37:43 +0000

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
   return (
     <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
       <div>
-        <h1 className="w-full text-3xl font-bold text-[#00df9a]">DNA.</h1>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">DATA</h1>
         <p className="py-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
